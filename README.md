dfd5a0b974adf5ae6816262b977f7cf585c311f3
Aaron Chang <mistayam94@gmail.com>
2018-10-13 16:53:53 -0700

    Initialize Splitagram

---
+++ README.md
@@ -0,0 +1,17 @@
+# Splitagram
+Split your images for instagram posts
+
+## Installing prerequisites
+```
+	pip install -r requirements.txt
+```
+
+## Running
+```
+ python main.py <fileName> <number of sections>
+```
+
+## Todo:
+[] Implement more robust argparsing
+[] Implement GUI (Tkinter)
+[] Automatically determine number of sections to split photo into for best instagram post
---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+import os
+from PIL import Image
+from math import floor
+# import re
+
+# fileName = re.match(r'.*[\.jpg]', sys.argv[1])
+
+def splitImage(numParts):
+    for x in range(numParts):
+        region = (getX(x, im.width, numParts), 0, getX(x+1, im.width, numParts), im.height)
+        section = im.crop(region)
+
+        file = ''.join([str(fileName), '/', str(fileName), '-', str(x), '.jpg'])
+        section.save(file)
+
+def getX(section, width, numParts):
+    return section * floor(width/numParts)
+
+#TODO: Make sure that file exists
+fileName = sys.argv[1].split('.')[0]
+
+#Save sections to directory
+if(os.path.exists(str(fileName)) == False):
+    os.mkdir(str(fileName))
+
+im = Image.open(sys.argv[1])
+splitImage(int(sys.argv[2]))
